fbe98d70973a4531671f96f9796da3e549a94f24
jimmyday12 <jamesthomasday@gmail.com>
2020-07-27 16:15:28 +1000

    adding pkgdown

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,7 +1,9 @@
 user <- Sys.getenv("MONASH_USER")
 pass <- Sys.getenv("MONASH_PASS")
 
+
 test_that("Get games works", {
+  
   expect_s3_class(get_games(user, pass, comp = "normal"), "data.frame")
   expect_s3_class(get_games(user, pass, comp = "gauss"), "data.frame")
   expect_s3_class(get_games(user, pass, comp = "info"), "data.frame")
