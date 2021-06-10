7cbad270dde5bdca7701ec75e31bbd71820a7edd
James Day <17557361+jimmyday12@users.noreply.github.com>
2021-06-10 16:09:23 +1000

    Update test-main.R

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,8 +1,20 @@
-user <- Sys.getenv("MONASH_USER")
-pass <- Sys.getenv("MONASH_PASS")
 
+
+skip_if_no_auth <- function() {
+  if (identical(Sys.getenv("MONASH_USER"), "")) {
+    skip("No authentication available")
+  }
+  if (identical(Sys.getenv("MONASH_PASS"), "")) {
+    skip("No authentication available")
+  }
+}
 
 test_that("Get games works", {
+  skip_if_offline()
+  skip_if_no_auth()
+  
+  user <- Sys.getenv("MONASH_USER")
+  pass <- Sys.getenv("MONASH_PASS")
   
   expect_s3_class(get_games(user, pass, comp = "normal"), "data.frame")
   expect_s3_class(get_games(user, pass, comp = "gauss"), "data.frame")
